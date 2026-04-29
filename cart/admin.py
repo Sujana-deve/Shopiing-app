63da2f8a1e797f2e5a4a01d8803b75b7ca18d25d
sujana <sharmasujana420@gmail.com>
2026-04-29 07:23:15 +0545

    cart features

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartItemInline(admin.TabularInline):
+    model  = CartItem
+    extra  = 0
+    fields = ['product', 'quantity', 'color_variant', 'size_variant']
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display  = ['user', 'get_cart_count', 'get_cart_total', 'is_paid']
+    list_filter   = ['is_paid']
+    search_fields = ['user__email']
+    inlines       = [CartItemInline]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'cart', 'quantity', 'color_variant', 'size_variant']
--- products/admin.py
+++ products/admin.py
@@ -1,27 +1,41 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage, ColorVariant, SizeVariant
 
-# Register your models here.
-from products.models import *
 
-admin.site.register(Category)
+class ProductImageInline(admin.TabularInline):
+    model  = ProductImage
+    extra  = 3   # shows 3 empty image slots by default
 
-class ProductImageAdmin(admin.StackedInline):
-    model = ProductImage
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['product_name','price']
-    inlines = [ProductImageAdmin]
+    list_display    = ['product_name', 'category', 'price', 'discount',
+                       'stock', 'is_featured']
+    list_editable   = ['price', 'discount', 'stock', 'is_featured']
+    list_filter     = ['category', 'is_featured']
+    search_fields   = ['product_name']
+    prepopulated_fields = {'slug': ('product_name',)}
+    inlines         = [ProductImageInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display        = ['category_name', 'slug']
+    prepopulated_fields = {'slug': ('category_name',)}
+
 
 @admin.register(ColorVariant)
 class ColorVariantAdmin(admin.ModelAdmin):
-    list_display = ['color_name','price']
-    model = ColorVariant
+    list_display  = ['color_name', 'price']
+    list_editable = ['price']
+
 
 @admin.register(SizeVariant)
 class SizeVariantAdmin(admin.ModelAdmin):
-    list_display = ['size_name','price']
-    model = SizeVariant
+    list_display  = ['size_name', 'price']
+    list_editable = ['price']
 
-admin.site.register(Product,ProductAdmin)
 
-admin.site.register(ProductImage)
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['product', 'image']
